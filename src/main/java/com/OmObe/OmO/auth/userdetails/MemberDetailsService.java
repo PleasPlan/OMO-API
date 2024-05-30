9c79d53292886e6d67581a0d56d3d9d14b67285e
seunghyun711 <seunghyun711@gmail.com>
2024-05-30 13:57:27 +0900

    [be/fix] : MemberDetailsService 주석 처리

--- src/main/java/com/OmObe/OmO/auth/userdetails/MemberDetailsService.java
+++ src/main/java/com/OmObe/OmO/auth/userdetails/MemberDetailsService.java
@@ -15,69 +15,69 @@ import java.util.Collection;
 import java.util.Optional;
 
 // db에서 조회한 user의 인증 정보를 기반으로 인증 처리
-@Component
-public class MemberDetailsService implements UserDetailsService {
-    private final MemberRepository memberRepository;
-    private final MemberAuthorityUtils authorityUtils;
-
-    public MemberDetailsService(MemberRepository memberRepository, MemberAuthorityUtils authorityUtils) {
-        this.memberRepository = memberRepository;
-        this.authorityUtils = authorityUtils;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        // 이메일을 통해 회원 조회
-        Optional<Member> optionalMember = memberRepository.findByEmail(username);
-        Member findMember = optionalMember.orElseThrow(() ->
-                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
-
-        return new MemberDetails(findMember);
-    }
-
-    private final class MemberDetails extends Member implements UserDetails {
-        MemberDetails(Member member) {
-            setMemberId(member.getMemberId());
-            setBirth(member.getBirth());
-            setClause(member.getClause());
-            setEmail(member.getEmail());
-            setGender(member.getGender());
-            setMbti(member.getMbti());
-            setMemberStatus(member.getMemberStatus());
-            setNickname(member.getNickname());
-            setPassword(member.getPassword());
-            setMemberRole(member.getMemberRole());
-        }
-
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return authorityUtils.createAuthorities(this.getMemberRole());
-        }
-
-        @Override
-        public String getUsername() {
-            return getEmail();
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return true;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return true;
-        }
-    }
-}
+//@Component
+//public class MemberDetailsService implements UserDetailsService {
+//    private final MemberRepository memberRepository;
+//    private final MemberAuthorityUtils authorityUtils;
+//
+//    public MemberDetailsService(MemberRepository memberRepository, MemberAuthorityUtils authorityUtils) {
+//        this.memberRepository = memberRepository;
+//        this.authorityUtils = authorityUtils;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        // 이메일을 통해 회원 조회
+//        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+//        Member findMember = optionalMember.orElseThrow(() ->
+//                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+//
+//        return new MemberDetails(findMember);
+//    }
+//
+//    private final class MemberDetails extends Member implements UserDetails {
+//        MemberDetails(Member member) {
+//            setMemberId(member.getMemberId());
+//            setBirth(member.getBirth());
+//            setClause(member.getClause());
+//            setEmail(member.getEmail());
+//            setGender(member.getGender());
+//            setMbti(member.getMbti());
+//            setMemberStatus(member.getMemberStatus());
+//            setNickname(member.getNickname());
+//            setPassword(member.getPassword());
+//            setMemberRole(member.getMemberRole());
+//        }
+//
+//        @Override
+//        public Collection<? extends GrantedAuthority> getAuthorities() {
+//            return authorityUtils.createAuthorities(this.getMemberRole());
+//        }
+//
+//        @Override
+//        public String getUsername() {
+//            return getEmail();
+//        }
+//
+//        @Override
+//        public boolean isAccountNonExpired() {
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean isAccountNonLocked() {
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean isCredentialsNonExpired() {
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean isEnabled() {
+//            return true;
+//        }
+//    }
+//}
