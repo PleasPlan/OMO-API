9f7e3bed53611cb6b09cd4a7bfb3c65e286f9081
seunghyun711 <seunghyun711@gmail.com>
2024-06-02 14:49:53 +0900

    [be/feat] : 닉네임 중복 검증 기능 추가

--- src/main/java/com/OmObe/OmO/member/controller/MemberController.java
+++ src/main/java/com/OmObe/OmO/member/controller/MemberController.java
@@ -54,4 +54,15 @@ public class MemberController {
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    // 닉네임 중복 검증
+    @PostMapping("/checkNickname")
+    public ResponseEntity checkNickname(@Valid @RequestBody MemberDto.NicknamePatch nickname){
+        // NicknamePatch를 통해 중복 여부를 검증할 닉네임을 받아온다.
+
+        // 닉네임 중복 검증 메서드 실행
+        memberService.verifyExistsNickname(nickname.getNickname());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
