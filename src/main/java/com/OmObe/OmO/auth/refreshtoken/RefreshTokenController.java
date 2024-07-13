870211fd671448db8f02479c3ca03d84dad555de
seunghyun711 <seunghyun711@gmail.com>
2024-07-13 12:12:30 +0900

    [be/fix] : 리프레시 토큰을 이용한 액세스 토큰 재발급 로직 수정

--- src/main/java/com/OmObe/OmO/auth/refreshtoken/RefreshTokenController.java
+++ src/main/java/com/OmObe/OmO/auth/refreshtoken/RefreshTokenController.java
@@ -8,26 +8,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class RefreshTokenController {
-    private final JwtTokenizer jwtTokenizer;
-    private final TokenService tokenService;
     private final RefreshTokenService refreshTokenService;
 
     // 리프레시 토큰을 통한 액세스 토큰 재발급
     @GetMapping("/reissueToken")
-    public ResponseEntity<HttpStatus> reissueAccessToken(HttpServletRequest request, HttpServletResponse response) {
-        String refreshToken = request.getHeader("Refresh");
+    public ResponseEntity<HttpStatus> reissueAccessToken(@RequestHeader("Refresh") String refreshToken,
+                                                         HttpServletResponse response) {
 
-        String accessToken = "Bearer " + refreshTokenService.reissueToken(refreshToken);
-        response.setHeader("Authorization", accessToken);
+        Map<String, String> reissuedTokens = refreshTokenService.reissueToken(refreshToken);
+
+        response.setHeader("Authorization", "Bearer " + reissuedTokens.get("accessToken"));
+        response.setHeader("Refresh", reissuedTokens.get("refreshToken"));
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
