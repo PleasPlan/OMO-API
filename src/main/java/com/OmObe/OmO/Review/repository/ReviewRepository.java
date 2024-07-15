62a47fc844a22fb537630d51407bcc2697ffb813
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-07-15 15:54:15 +0900

    feat: reviewRepository 변경
    reviewRepository에 member와 placeId로 찾을 수 있도록 추가

--- src/main/java/com/OmObe/OmO/Review/repository/ReviewRepository.java
+++ src/main/java/com/OmObe/OmO/Review/repository/ReviewRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>, JpaSpecificationExecutor<Review>{
     Optional<List<Review>> findByPlaceId(long placeId);
+    Optional<Review> findByMemberAndPlaceId(long memberId, long placeId);
 }
