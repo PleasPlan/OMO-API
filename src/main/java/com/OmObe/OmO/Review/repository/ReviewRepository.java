ade14c5acab7e3a97e5491781f54406d1665789f
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-07-15 16:07:19 +0900

    fix: reivewRepository 변경
    이름 및 내부 매개변수 조정

--- src/main/java/com/OmObe/OmO/Review/repository/ReviewRepository.java
+++ src/main/java/com/OmObe/OmO/Review/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.OmObe.OmO.Review.repository;
 
 import com.OmObe.OmO.Review.entity.Review;
+import com.OmObe.OmO.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,5 @@ import java.util.Optional;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>, JpaSpecificationExecutor<Review>{
     Optional<List<Review>> findByPlaceId(long placeId);
-    Optional<Review> findByMemberIdAndPlaceId(long memberId, long placeId);
+    Optional<Review> findByMemberAndPlaceId(Member member, long placeId);
 }
