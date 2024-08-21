f1381cc51a2e4823e43a8e2e475588425ef3ad07
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-08-21 19:16:35 +0900

    feat: 나만의 코스 작성자 조회유저 비교
    나만의 코스에서 작성자와 조회한 유저가 같은 유저인지 비교하는 writerUserMatch 새로 리턴해줌.

--- src/main/java/com/OmObe/OmO/MyCourse/dto/MyCourseDto.java
+++ src/main/java/com/OmObe/OmO/MyCourse/dto/MyCourseDto.java
@@ -67,6 +67,7 @@ public class MyCourseDto {
         private String writerName;
         private Boolean myLiked;
         private Boolean share;
+        private Boolean writerUserMatch;
     }
 
 
