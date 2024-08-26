2a059c668261cac0b949c9f588bc8d91b0131325
seunghyun711 <seunghyun711@gmail.com>
2024-08-26 14:52:59 +0900

    [be/fix] : 각 신고 엔티티의 id 필드를 reportId로 변경

--- src/main/java/com/OmObe/OmO/report/commentreport/entity/CommentReport.java
+++ src/main/java/com/OmObe/OmO/report/commentreport/entity/CommentReport.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class CommentReport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long commentReportId;
+    private Long reportId;
 
     /*
     <신고 유형>
